52d833a8549b8a15ba38c2f259e3ac31ad12ef3a
yaoyao <liuyao@163.com>
2021-12-22 19:55:05 +0800

    update xml

--- src/main/java/io/yaoyao/HelloWorld.java
+++ src/main/java/io/yaoyao/HelloWorld.java
@@ -10,38 +10,38 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class HelloWorld extends AbstractHandler {
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
-            throws IOException, ServletException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
-    }
-
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(9090);
-        server.setHandler(new HelloWorld());
-
-        server.start();
-        server.join();
-    }
-}
-
-//import java.io.IOException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
+//public class HelloWorld extends AbstractHandler {
+//    public void handle(String target,
+//                       Request baseRequest,
+//                       HttpServletRequest request,
+//                       HttpServletResponse response)
+//            throws IOException, ServletException {
+//        response.setContentType("text/html;charset=utf-8");
+//        response.setStatus(HttpServletResponse.SC_OK);
+//        baseRequest.setHandled(true);
+//        response.getWriter().println("<h1>Hello World</h1>");
+//    }
 //
-//@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
-//public class HelloWorld extends HttpServlet {
-//    @Override
-//    public void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws IOException {
-//        response.getOutputStream().print("Hello World");
+//    public static void main(String[] args) throws Exception {
+//        Server server = new Server(9090);
+//        server.setHandler(new HelloWorld());
+//
+//        server.start();
+//        server.join();
 //    }
 //}
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
+public class HelloWorld extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.getOutputStream().print("Hello World");
+    }
+}
